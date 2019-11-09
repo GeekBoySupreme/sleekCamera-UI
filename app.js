e2123e116f627b11e0beebf424b09fcbd73fb06d
geekboysupreme <shuvam.manna@gmail.com>
2019-11-09 15:44:37 +0530

    Base64 to Blob

--- app.js
+++ app.js
@@ -24,14 +24,48 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    //console.log( cameraOutput.src);
+    cameraOutput.src = cameraSensor.toDataURL("image/png");
+    //console.log(cameraOutput.src);
 
     var dataURL = cameraSensor.toDataURL();
-    console.log(dataURL);
+    //console.log(dataURL);
 
+    const contentType = 'image/png';
+    const b64Data = dataURL.substring(22, (dataURL.length-1));
+
+    const blob = b64toBlob(b64Data, contentType);
+    const blobUrl = URL.createObjectURL(blob);
+
+    console.log(blobUrl);  
+
+    /*fetch(dataURL)
+    .then(res => res.blob())
+    .then(console.log)*/
+    
     cameraOutput.classList.add("taken");
 };
+
+
+//converting base64 to blob
+const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
+    const byteCharacters = atob(b64Data);
+    const byteArrays = [];
+  
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+  
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+  
+      const byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+      
+    const blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+  }
 
 
 // Start the video stream when the window loads
